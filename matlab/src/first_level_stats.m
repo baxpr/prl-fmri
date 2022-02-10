617d3a6a55b382f791928ce3ee845744e1864d82
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-02-10 08:58:19 -0600

    warm

---
+++ matlab/src/first_level_stats.m
@@ -0,0 +1,7 @@
+function first_level_stats(inp)
+
+%   Model T1_TrialStart, T2b_CardFlipOnset
+%     (1) WinStay, WinSwitch, Lose   x   Easy, Hard
+%     (2) Win, Lose                  x   Easy, Hard
+
+% Include motion params in model?
