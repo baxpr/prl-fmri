574b1c95a23fc8600988e8b01b44cfe0d6a16ae7
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-12-09 14:11:37 -0600

    testcode

---
+++ test_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+docker run \
+    --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
+    --mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
+    prl-fmri:test \
+    --fmri1_niigz /INPUTS/fmri1.nii.gz \
+    --fmri2_niigz /INPUTS/fmri2.nii.gz \
+    --fmri3_niigz /INPUTS/fmri3.nii.gz \
+    --fmri4_niigz /INPUTS/fmri4.nii.gz \
+    --fmritopup_niigz /INPUTS/fmritopup.nii.gz \
+    --seg_niigz /INPUTS/seg.nii.gz \
+    --icv_niigz /INPUTS/icv.nii.gz \
+    --deffwd_niigz /INPUTS/y_deffwd.nii.gz \
+    --biascorr_niigz /INPUTS/biascorr.nii.gz \
+    --biasnorm_niigz /INPUTS/biasnorm.nii.gz \
+    --trialreport_csv /INPUTS/trialreport.csv \
+    --hpf_sec 300 \
+    --fwhm_mm 6 \
+    --out_dir /OUTPUTS
+
